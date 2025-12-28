935bbeb02ad0a638f8dcb0af36b2bfbe9fa6d375
Tenzin-06 <zintentobgay06@gmail.com>
2025-12-28 15:22:14 +0530

    blogs and search feature

---
+++ assignment/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class About(models.Model):
+    about_heading = models.CharField(max_length=25)
+    about_description = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "About"
+
+    def __str__(self):
+        return self.about_heading
+    
+class SocialLink(models.Model):
+    platform_name = models.CharField(max_length=50)
+    profile_url = models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.platform_name
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from blogs.models import Category, Blog
+from assignment.models import About, SocialLink
 
 def home(request):
     categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True)
     posts = Blog.objects.filter(is_featured = False, status='Published')
 
+    #Fetch about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+
     context = {
         'categories': categories,
         'featured_posts': featured_posts,
         'posts': posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
